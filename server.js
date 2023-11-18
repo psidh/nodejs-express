abf6e575140d732eb7a390ebb434e4bd66e09525
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-11-19 00:34:58 +0530

    assets <- public

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 });
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'assets')));
 
 // Handle form submission
 app.post('/submit', upload.none(), (req, res) => {
