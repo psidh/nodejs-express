e4f98531575b6e4a65df6cfc62d9c94ab1ab0c08
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-11-18 23:58:56 +0530

    headed back to 1

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 3000;
 const multer = require('multer');
 const upload = multer();
+const path = require('path');
+
 // Parse application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
@@ -15,19 +17,24 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Handle form submission
 app.post('/submit', upload.none(), (req, res) => {
     console.log('Body:', req.body);
 
     let name = req.body.name?.toString();
+    let domain = req.body?.domain.toString();
     let techStack = req.body.techStack?.toString();
 
     console.log('Name:', name);
+    console.log('Domain', domain);
     console.log('Tech Stack:', techStack);
         
     res.json({
         success: true,
-        user: { name, techStack }
+        user: { name, domain, techStack }
     });
 });
 
@@ -35,3 +42,4 @@ app.post('/submit', upload.none(), (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
+
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    // Add more paths as needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
